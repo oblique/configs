249e6a7ea8e12591c71bf85cac722c96c40f3b28
oblique <psyberbits@gmail.com>
2012-06-30 22:52:57 +0300

    update #153

--- .urxvt/utubify.sh
+++ .urxvt/utubify.sh
@@ -3,15 +3,15 @@
 # Simple script to use youtube-dl for all youtube links
 # and your browser of choice otherwise
 
-LINK=$@
 BROWSER=firefox
+export LINK=$@
 
 if [ -z ${LINK} ]; then
     echo "usage: $0 <link>"
     exit 1
 fi
 
-echo ${LINK} | grep -ie "https\?://\(\(\(www.\)\?youtube.com/.*\(?\|&\)v=.\+\)\|\(youtu.be/.\+\)\)"
+echo ${LINK} | grep -ie "https\?://\(\(\(www.\)\?youtube.com/watch\(?\|.*&\)v=.\+\)\|\(youtu.be/.\+\)\)"
 if [ $? -eq 0 ]; then
     urxvt -e sh -c 'mplayer $(youtube-dl -g ${LINK})' &
 else
