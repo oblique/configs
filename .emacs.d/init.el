ef9a4745160932308b55429a65f6a492fcf4020d
oblique <psyberbits@gmail.com>
2013-03-29 00:51:56 +0200

    emacs: change fonts size in gui

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -94,7 +94,7 @@
 
 ; fonts
 (if window-system
-    (setq default-frame-alist '((font . "Source Code Pro-8"))))
+    (setq default-frame-alist '((font . "Source Code Pro-9"))))
 
 ; theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
