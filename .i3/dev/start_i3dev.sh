e06423c63d0f96591d4231ba25b894a0a33fbc50
oblique <psyberbits@gmail.com>
2013-06-21 14:17:46 +0300

    i3 dev scripts: dual-head environment

--- .i3/dev/start_i3dev.sh
+++ .i3/dev/start_i3dev.sh
@@ -4,6 +4,9 @@ if [[ ! -d ~/.i3/dev/git/i3 || ! -d ~/.i3/dev/git/i3status ]]; then
     exit 1
 fi
 
-~/.i3/bin/i3-exec-wait.pl ~/.i3/dev/start_xephyr.sh
+~/.i3/bin/i3-exec-wait.pl -n 2 ~/.i3/dev/start_xephyr.sh
+i3-msg layout splith
+i3-msg focus parent
+i3-msg split v
 ~/.i3/bin/i3-exec-wait.pl ~/.i3/dev/start_term.sh
 ~/.i3/bin/i3-exec-wait.pl ~/.i3/dev/start_i3.sh
--- .i3/dev/start_xephyr.sh
+++ .i3/dev/start_xephyr.sh
@@ -4,4 +4,4 @@ if [[ ! -d ~/.i3/dev/git/i3 || ! -d ~/.i3/dev/git/i3status ]]; then
     exit 1
 fi
 
-exec Xephyr :1 -ac -screen 956x540 -noreset
+exec Xephyr +xinerama -screen 956x510 -origin 956x0 -screen 956x510 -ac -noreset :1
