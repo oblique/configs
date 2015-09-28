39c05b2cd7d6a0c617a1e2ef59b677bca2d1ba58
oblique <psyberbits@gmail.com>
2015-09-28 23:13:24 +0300

    Switch completely to `rofi`

--- .i3/bin/exit_menu.sh
+++ .i3/bin/exit_menu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-while [ "$select" != "NO" -a "$select" != "YES" ]; do
-    select=$(echo -e 'NO\nYES' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.")
-    [ -z "$select" ] && exit 0
+while [[ "$select" != "NO" && "$select" != "YES" ]]; do
+    select=$(echo -e 'NO\nYES' | rofi -color-normal \
+        '#101010,#f00060,#131313,#d10047,#101010' \
+        -dmenu -i -p "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.")
+    [[ -z "$select" ]] && exit 0
 done
-[ "$select" = "NO" ] && exit 0
+[[ "$select" = "NO" ]] && exit 0
 i3-msg exit
