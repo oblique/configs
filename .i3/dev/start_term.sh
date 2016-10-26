78b6c28345ff330759e66069ed128eaa275796d2
oblique <psyberbits@gmail.com>
2016-10-26 20:26:04 +0300

    Remove i3 dev scripts

--- .i3/dev/start_term.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ ! -d ~/.i3/dev/git/i3 || ! -d ~/.i3/dev/git/i3status ]]; then
-    echo "run ~/.i3/dev/mki3git.sh first."
-    exit 1
-fi
-
-unset TMUX
-urxvt -e bash -c 'cd ~/.i3/dev; DISPLAY=:1.0 tmux' &
--- .i3/dev/start_xephyr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ ! -d ~/.i3/dev/git/i3 || ! -d ~/.i3/dev/git/i3status ]]; then
-    echo "run ~/.i3/dev/mki3git.sh first."
-    exit 1
-fi
-
-exec Xephyr +xinerama -screen 956x510 -origin 956x0 -screen 956x510 -ac -noreset :1
