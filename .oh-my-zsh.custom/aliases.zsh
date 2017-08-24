b1780b8f04282866cbc5f28027d719074dc93f2a
oblique <psyberbits@gmail.com>
2017-08-24 09:15:27 +0300

    zsh: Add `youtube-dl-mp3` alias

--- .oh-my-zsh.custom/aliases.zsh
+++ .oh-my-zsh.custom/aliases.zsh
@@ -4,3 +4,4 @@ alias shred='shred -n 10 -u -v -z --random-source /dev/urandom'
 alias extip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 alias HandBrakeCLI_HD="HandBrakeCLI -e x264 -2 -T -q 18.0 -a 1,1 -E faac,copy:ac3 -B 160,160 -6 dpl2,auto -R Auto,Auto -D 0.0,0.0 -4 --decomb --strict-anamorphic --crop 0:0:0:0 -m -x level=41:b-adapt=1:rc-lookahead=50:me=umh:trellis=2"
+alias youtube-dl-mp3='youtube-dl --extract-audio --audio-format mp3'
