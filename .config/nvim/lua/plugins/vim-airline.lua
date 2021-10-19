b046e0be46a9659cd9abc7d8f7b0cd42def94b40
oblique <psyberbits@gmail.com>
2021-10-20 01:43:46 +0300

    nvim: Update vim-airline config

--- .config/nvim/lua/plugins/vim-airline.lua
+++ .config/nvim/lua/plugins/vim-airline.lua
@@ -1,9 +1,9 @@
 vim.g.airline_theme = 'darkness'
 vim.g.airline_powerline_fonts = true
 vim.g['airline#extensions#tabline#enabled'] = true
-
--- Show just the filename
-vim.g['airline#extensions#tabline#fnamemod'] = ':t'
+vim.g['airline#extensions#nvimlsp#enabled'] = true
+-- Show filename and parent directory
+vim.g['airline#extensions#tabline#formatter'] = 'short_path'
 vim.opt.laststatus = 2
 -- we don't need mode since airline show it
 vim.opt.showmode = false
