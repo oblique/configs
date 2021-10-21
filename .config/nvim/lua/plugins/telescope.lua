814ec7dc780054261e7b5b3e7ddd1908530a224a
oblique <psyberbits@gmail.com>
2021-10-21 22:05:39 +0300

    nvim: Add telescope scrolling keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,22 @@
 local telescope = require('telescope')
 local telescope_builtin = require('telescope.builtin')
+local telescope_actions = require('telescope.actions')
 local nest = require('nest')
 
-telescope.setup()
+telescope.setup {
+    defaults = {
+        mappings = {
+            i = {
+                ['<C-Up>'] = telescope_actions.preview_scrolling_up,
+                ['<C-Down>'] = telescope_actions.preview_scrolling_down,
+            },
+            n = {
+                ['<C-Up>'] = telescope_actions.preview_scrolling_up,
+                ['<C-Down>'] = telescope_actions.preview_scrolling_down,
+            },
+        }
+    }
+}
 
 nest.applyKeymaps {
     {
