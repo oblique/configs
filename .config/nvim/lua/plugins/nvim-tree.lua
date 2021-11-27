861ce5126d86e50052067c292021abb3baf660c9
oblique <psyberbits@gmail.com>
2021-11-27 17:20:54 +0200

    nvim: Update nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,11 @@
--- nvim_tree variables should be set before `require`
-vim.g.nvim_tree_hide_dotfiles = 1
-
 local nvim_tree = require('nvim-tree')
 local nest = require('nest')
 
-nvim_tree.setup()
+nvim_tree.setup {
+    filters = {
+        dotfiles = true,
+    }
+}
 
 nest.applyKeymaps {
     { '<leader>n', '<cmd>NvimTreeToggle<cr>' },
