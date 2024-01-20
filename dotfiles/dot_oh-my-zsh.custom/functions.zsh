60a82ed2ff81c9d37d3747edbeb9cbff20f5c324
Yiannis Marangos <psyberbits@gmail.com>
2024-01-20 12:06:31 +0200

    Update zsh config

--- dotfiles/dot_oh-my-zsh.custom/functions.zsh
+++ dotfiles/dot_oh-my-zsh.custom/functions.zsh
@@ -22,7 +22,7 @@ extip() {
     echo
 }
 
-clean-all-cargo() {
+cargo-clean-all() {
     fd -I -s '^Cargo.toml$' | while read -r x; do
         dir="$(dirname "$x")"
 
@@ -31,4 +31,33 @@ clean-all-cargo() {
             rm -rf "$dir/target"
         fi
     done
+}
+
+# Run this after `git fetch -p` to clean the local branches that
+# were tracked by already delete remote branches.
+git-prune-gone() {
+    local dry_run=0
+
+    if [[ $# -gt 1 ]]; then
+        echo "usage: git-prune-gone [-n | --dry-run]"
+        return 1
+    fi
+
+    if [[ "$1" == "-n" || "$1" == "--dry-run" ]]; then
+        dry_run=1
+    fi
+
+    if [[ $dry_run -eq 1 ]]; then
+        git branch -vv | awk '/: gone/{print $1}'
+    else
+        git branch -vv | awk '/: gone/{print $1}' | xargs git branch -D
+    fi
+}
+
+# Workaround for cargo-update until sparse indices get supported.
+#
+# Ref: https://github.com/killercup/cargo-edit/issues/869
+cargo-upgrade() {
+    CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo fetch
+    CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo upgrade
 }
